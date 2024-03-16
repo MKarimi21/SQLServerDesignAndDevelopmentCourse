49f0b5f59d786a4a91769bf11860e2a56cb568ab
Mostafa Karimi <mkarimi21@hotmail.com>
2024-03-16 12:26:59 +0330

    create trigger on table level

--- Exercise/Session1302.sql
+++ Exercise/Session1302.sql
@@ -29,3 +29,30 @@ DROP TRIGGER MessageAfterCreatingDataBase ON ALL SERVER;
 GO
 ;
 
+-- Trigegr on table - usefull for logging and security
+USE DB1;
+
+CREATE TABLE dbo.People_DeleteLog (
+    Id INT IDENTITY(1, 1),
+    Delete_PeopleId CHAR(10),
+    ModifiedDate DATETIME DEFAULT GETDATE()
+);
+
+GO
+;
+
+CREATE TRIGGER DeleteLog ON dbo.People
+AFTER
+    DELETE AS BEGIN
+SET
+    NOCOUNT ON;
+
+INSERT
+    dbo.People_DeleteLog (Delete_PeopleId)
+SELECT
+    D.National_Identity
+FROM
+    deleted D
+END
+GO
+;
