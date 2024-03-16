9865e686c7418597f76136a0725a6531b62e5299
Mostafa Karimi <mkarimi21@hotmail.com>
2024-03-16 20:17:50 +0330

    add transaction querry

--- Exercise/Session1402.sql
+++ Exercise/Session1402.sql
@@ -1,2 +1,21 @@
 -- Transaction
 -- ACID => Atomic, Consistent, Isolates, Durable
+SET
+    XACT_ABORT ON;
+
+-- baes mishe harja be moshkel khordim jaryan ro stop va zakhireharo hazf mikone
+BEGIN TRAN
+INSERT
+    dbo.Orders (Customer, OrderDate)
+VALUES
+    ('Mosi', GETDATE());
+
+DECLARE @Local_Id INT = @SCOP_IDENTITY();
+
+INSERT
+    dbo.OrderDetals (Order_Id, Product, Quantity, Discount)
+VALUES
+    (@Local_Id, 'Bike', 1, 0.2),
+    (@Local_Id, 'Tire', 1, 0.1) -- if it has wrong value then querry stop
+    COMMIT;
+
